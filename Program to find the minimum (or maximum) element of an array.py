bf7885cc35ee1a1d3d8cf1fc9b3737b0ab57c612
aditya6473 <62197248+aditya6473@users.noreply.github.com>
2021-01-29 11:29:39 +0530

    Add files via upload

---
+++ Program to find the minimum (or maximum) element of an array.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def getMin(arr, n):
+    res = arr[0]
+    for i in range(1,n):
+        res = min(res, arr[i])
+    return res
+ 
+
+def getMax(arr, n):
+    res = arr[0]
+    for i in range(1,n):
+        res = max(res, arr[i])
+    return res
+ 
+
+arr = [12, 1234, 45, 67, 1]
+n = len(arr)
+print ("Minimum element of array:", getMin(arr, n))
+print ("Maximum element of array:", getMax(arr, n))
+
+
+# In[ ]:
+
+
+
+
